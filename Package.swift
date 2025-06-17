5f2927a2e57e00fbb22aef6ca6cdb676814ce46e
Ugo Stephant <ugo@poool.fr>
2025-06-17 14:46:23 +0200

    chore: update Package.swift release checksum for v1.2.1

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "EngageIOS",
-            url: "https://github.com/p3ol/engage-package-ios/releases/download/v1.1.2/EngageIOS.xcframework.zip",
-            checksum: "3b1d0b674142e476d9bc5bc02c9120f8a145f36bb5b59ff52668b7a9d1ab9bc2"
+            url: "https://github.com/p3ol/engage-package-ios/releases/download/v1.2.2/EngageIOS.xcframework.zip",
+            checksum: "d09cb0b3b763448bee622487da40d418d054b94879a051c12a0524bc1585cc2a"
         ),
     ]
 )
